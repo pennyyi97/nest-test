3f14359e7e48393a3a2e570b1fa383971c6563b4
pennyyi97 <pennyyi97@gmail.com>
2026-01-20 10:10:22 +0900

    drizzle orm CRUD 실습

--- nest-drizzle/src/post/post.service.ts
+++ nest-drizzle/src/post/post.service.ts
@@ -23,7 +23,8 @@ export class PostService {
       .leftJoin(users, eq(posts.authorId, users.id))
       .leftJoin(usersToGroups, eq(posts.authorId, usersToGroups.userId))
       .leftJoin(groups, eq(usersToGroups.groupId, groups.id));
-    // .where(eq(posts.id, 20));
+
+    console.log('전체 조회');
     return rows.map((r) => ({
       ...r.posts,
       author: {
@@ -33,17 +34,23 @@ export class PostService {
     }));
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
+  async findOne(id: number) {
+    console.log(`${id} post 조회`);
+
+    return await this.db
+      .select({
+        id: posts.id,
+        title: posts.title,
+        content: posts.content,
+        author: users.name,
+      })
+      .from(posts)
+      .innerJoin(users, eq(posts.authorId, users.id))
+      .where(eq(posts.id, id));
   }
 
   async update(id: number, updatePostDto: UpdatePostDto) {
-    return await this.db
-      .update(posts)
-      .set({
-        title: 'AAAAAAA',
-      })
-      .where(eq(posts.id, id));
+    return await this.db.update(posts).set(updatePostDto).where(eq(posts.id, id));
   }
 
   async remove(id: number) {
